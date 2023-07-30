1673eb8f8e8e631aa0fd39d63ba6216bf17061a9
Mati <mati@MacBook-Pro-de-Mati.local>
2023-07-30 11:47:53 -0300

    added prettier/eslint rules

--- template/.eslintrc.js
+++ template/.eslintrc.js
@@ -1,4 +1,10 @@
 module.exports = {
   root: true,
   extends: '@react-native',
-};
+  rules: {
+    semi: ['error', 'never'],
+    'prettier/prettier': ['error', { bracketSameLine: false }],
+    'react-native/no-inline-styles': 'error',
+    '@typescript-eslint/no-explicit-any': 'error',
+  },
+}
